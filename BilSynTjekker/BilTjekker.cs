a78bc0d577627f7b0bcf0055ab5683ec4340deca
Tec <Tec@SKAB4-PC2>
2023-04-25 09:05:13 +0200

    Add project files.

---
+++ BilSynTjekker/BilTjekker.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilSynTjekker;
+
+/// <summary>
+/// Bil klassen som objekt, den har fire parameter som skal udfyldes vil oprettelse af ny Bil
+/// </summary>
+internal class Bil
+{
+    public string? mærke;
+    public string? model;
+    public int årgang;
+    public string? sidstSynet;
+    public Bil(string? Mærke, string? Model, int Årgang, string? SidstSynet)
+    {
+        mærke = Mærke;
+        model = Model;
+        årgang = Årgang;
+        sidstSynet = SidstSynet;
+    }
+}
+
+/// <summary>
+/// Struct klassen til at have alle metoder der laver en eller anden slags udregning med parameter taget fra Program.cs
+/// </summary>
+internal struct Udregninger
+{
+    // DatoSplit metoden bruges til at udregne mængden af tid siden bilen har været til syn og om den skal igen.
+    public static bool DatoSplit(string? indputDato, int årgang)
+    {
+        // Først splittes indputDato til dag, måned og år.
+        string[] datoSplit = indputDato.Split('.');
+        string dag = datoSplit[0];
+        string måned = datoSplit[1];
+        string år = datoSplit[2];
+
+        // Herefter sammles de igen i det format som DateTime Datatypen forstår, "dt1" bliver variablen for den indtastet dato mens "dt2" bliver den aktuelle dag idag, variiablen "detteÅr" tager så info omkring årstallet fra "dt2"
+        string dato = måned + "/" + dag + "/" + år;
+        DateTime dt1 = DateTime.Parse(dato);
+        DateTime dt2 = DateTime.Now;
+        string detteÅr = dt2.Year.ToString();
+
+        // De bilver begge brugt til at udregne forskellen mellem datoerne og mellem årstal individuelt
+        var datoForskel = dt2.Subtract(dt1);
+        int årForskel = Convert.ToInt32(detteÅr) - årgang;
+
+        // Hvis "årForskel" er 5 eller mere vil programmet gå ind i følgende if sætning, da det betyder at bilen er mere end fem år gammel
+        // Herefter bruges "datoForskel" til at tjekke om bilen har været til syn inde for de sidste to år (730 dage) og den korrekte bool værdi returneres til programmet
+        if (årForskel >= 5)
+        {
+            if (datoForskel.Days >= 730)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    // TjekTegn metoden bruges til at tjekke om indtastet dato skrives på den bestemte måde jeg vil have, altså opdelt af "."
+    public static bool TjekTegn(string x)
+    {
+        // En Try catch bruges til at tjekke om den indtastet dato kan splittes op ved brug af ".", hvis den kan returneres "true", hvis ikke returneres "false"
+        try
+        {
+            string[] datoSplit = x.Split('.');
+            string dag = datoSplit[0];
+            string måned = datoSplit[1];
+            string år = datoSplit[2];
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+
+    // FejlTjek metoden bruges bare til at sikre at bruges indput til om det vil søge igen konkret er "y" eller "n" ellers returneres "false" og metoden køres igen
+    public static bool FejlTjek(string x)
+    {
+        if (x == "y" || x == "n")
+        {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ BilSynTjekker/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using BilSynTjekker;
+
+namespace BilSynTjekker;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        // De to mulig biler med farbriksfej oprettet som "Bil" objekter
+        Bil Fiat = new Bil("Fiat", "Punto", 2010, "");
+        Bil Alfa = new Bil("Alfa Romeo", "Giulia", 2019, "");
+
+        // Samme to biler indsat i et array for at kunne måle brugerindput imod dem
+        Bil[] fejlbiler = new Bil[2];
+        fejlbiler[0] = Fiat;
+        fejlbiler[1] = Alfa;
+
+        // Flag som sørge for at do while loop bliver ved med at køre indtil brugeren vælger nej til at "Søge igen"
+        bool flag = true;
+        // do while loop som hele programmet udfoldes inden i
+        do
+        {
+            // De fire parameter for brugerindput initialiseret
+            string? Mærke = "";
+            string? Model = "";
+            int Årgang;
+            string? SidstSynet = "";
+
+            // Her spørg programmet brugeren om de tilsvarende oplysninger omkring bilen og de indlæses i variablerne
+            Console.WriteLine("- x - Bil Syns Tjekker - x -\n");
+            Console.Write("Indput venligst bilens Mærke: ");
+            Mærke = Console.ReadLine();
+            Console.Write("Indput venligst bilens Model: ");
+            Model = Console.ReadLine();
+            Console.Write("Indput venligst bilens Årgang: ");
+            Årgang = Convert.ToInt32(Console.ReadLine());
+
+            // Loop til fejltjekning af om brugenen indtaster dato på den bestemte måde (Her vil jeg have at de indtaster den spredt med et "."
+            bool TjekTegn = true;
+            do
+            {
+                // Hvis den senere metode "TjekTegn" kommer tilbage som false vil do while loopet køre igen og denne fejlbesked vil blive udskrevet
+                if (TjekTegn == false)
+                {
+                    Console.WriteLine("Fejl, indput dato delt op af \".\"");
+                }
+
+                Console.Write("Indput venligst bilens sidste synet dato(Brug \".\" til at opdele dag, måned og år): ");
+                SidstSynet = Console.ReadLine();
+                // Metode til at tjekke om dato er skrevet rigtigt
+                TjekTegn = Udregninger.TjekTegn(SidstSynet);
+            } while (TjekTegn == false);
+
+            // Metode til at indtaste og udregne om bilen
+            // Udregninger.DatoSplit(SidstSynet, Årgang);
+
+            // Oprettelse af brugerindput som "Bil" objekt
+            Bil bil = new Bil(Mærke, Model, Årgang, SidstSynet);
+
+            // For-loop til at gennemtjekke array for mulige ligheder til brugens indput
+            for (int i = 0; i < fejlbiler.Length; i++)
+            {
+                // Hvis det indputtet mærke, model samt årgang matcher med info fra array vil programmet gå ind i denne if sætning
+                if (fejlbiler[i].mærke == bil.mærke && fejlbiler[i].model == bil.model && fejlbiler[i].årgang <= bil.årgang)
+                {
+                    // I forhold til hvilke informationer der matchede vil den relevante af nedenstående to fejlbeskeder blive udskrevet.
+                    if (bil.mærke == Alfa.mærke)
+                    {
+                        Console.WriteLine("Bilen har følgende fabriksfejl: Udstødning ");
+                    }
+                    else if (bil.mærke == Fiat.mærke)
+                    {
+                        Console.WriteLine("Bilen har følgende fabriksfejl: Styretøjet ");
+                    }
+                } // Hvis indtastede informationer ikke matcher med noget info fra array vil programmet i stedet fortsætte her.
+                else
+                {
+                    // Metoden "Udregninger.DatoSplit" tager de relevante parameter ind (SidstSynet og Årgang) og udregner om bilen skal til syn eller ej. 
+                    // Der er taget højde for om bilen er mere end 5 år gammel og efterfølgende om bilen har været til syn inden for de sidste to år 
+                    bool SynTjek = Udregninger.DatoSplit(SidstSynet, Årgang);
+
+                    // If sætning til udskriving af resultat om bilens skal til syn eller ej
+                    if (SynTjek == true)
+                    {
+                        Console.WriteLine("Bilen skal til syn");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Bilen skal ikke synes");
+                        break;
+                    }
+                }
+            }
+
+            // Søg igen funktion så brugeren ikke skal genåbne programmet hver gang
+            Console.WriteLine("Vil du søge igen? [y/n]");
+            // Initialisere variablen FejlTjek
+            bool FejlTjek;
+            do
+            {
+                // Tager brugerens indput og laver det "ToLower" 
+                string Igen = Console.ReadLine().ToLower();
+                // Metode til at tjekke om brugerens indput enten er Y eller N
+                FejlTjek = Udregninger.FejlTjek(Igen);
+                // If sætning til at få programmet til at reagere på den ønskede måde til brugerens indput
+                if (FejlTjek == false)
+                {
+                    Console.WriteLine("Indput venligst y for Ja eller n for Nej");
+                }
+                else if (Igen == "y")
+                {
+                    Console.Clear();
+                }
+                else if (Igen == "n")
+                {
+                    flag = false;
+                }
+            } while (FejlTjek == false);
+
+
+        } while (flag == true);
+    }
+
+}
